cba3bd6bb29af12ec340aec4f144ab6e8902c6ed
Justin Azoff <justin@bouncybouncy.net>
2011-07-22 10:08:13 -0400

    implement config file reading and caching

--- ninfo/__init__.py
+++ ninfo/__init__.py
@@ -1,6 +1,16 @@
 import sys
 from pkg_resources import iter_entry_points
 
+import memcache
+
+import logging
+logger = logging.getLogger("ninfo")
+
+import os
+import ConfigParser
+
+from mako.template import Template
+
 class PluginBase(object):
 
     cache_timeout = 60*60
@@ -19,12 +29,30 @@ class PluginBase(object):
         return self.get_info(arg)
 
 class Ninfo:
-    def __init__(self):
+    def __init__(self, config_file=None):
         self.plugin_modules = {}
         for ep in iter_entry_points(group='ninfo.plugin'):
             self.plugin_modules[ep.name] = ep
         self.plugins = {}
         self.plugin_instances = {}
+
+        self.read_config(config_file)
+        self.cache_host = self.config['general'].get('memcache_host')
+        if self.cache_host:
+            self.cache = memcache.Client([self.cache_host])
+        else:
+            self.cache = None
+
+    def read_config(self, config_file):
+        cp = ConfigParser.ConfigParser()
+        if config_file:
+            cp.read([config_file])
+        elif os.getenv("INFO_CONFIG_FILE"):
+            cp.read([os.getenv("INFO_CONFIG_FILE")])
+        else:
+            cp.read(["ninfo.ini","/etc/ninfo.ini"])
+        #return a simple nested dictionary structure from the config
+        self.config = dict((s, dict(cp.items(s))) for s in cp.sections())
 
     def get_plugin(self, plugin):
         if plugin in self.plugins:
@@ -43,5 +71,20 @@ class Ninfo:
         return instance
 
     def get_info(self, plugin, arg):
-        instance = self.get_inst(plugin)
-        return instance.get_info(arg)
+
+        if self.cache:
+            KEY = 'ninfo:%s:%s' % (plugin, arg)
+            ret = self.cache.get(KEY)
+            if ret:
+                return ret[1]
+
+        try:
+            instance = self.get_inst(plugin)
+            ret = instance.get_info(arg)
+            if self.cache:
+                timeout = self.get_plugin(plugin).cache_timeout
+                self.cache.set(KEY, (True, ret), timeout)
+            return ret
+        except Exception, e:
+            logger.exception("Error running plugin %s" % plugin)
+            raise
