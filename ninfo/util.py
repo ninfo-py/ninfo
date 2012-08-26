c2d48df0ed00707fdad2f4ddd732f2b49cb055b4
Justin Azoff <justin@bouncybouncy.net>
2012-08-26 16:44:12 -0400

    add fmt_dict_array helper function

--- ninfo/util.py
+++ ninfo/util.py
@@ -66,3 +66,43 @@ def parse_query(s):
         else:
             args.append(arg)
     return args, options
+
+def fmt_dict_array(ar, order=None, header=True):
+    #allow for an empty array
+    #if the array is empty and no order is paseed, return ""
+    #otherwise generate just the header
+    if not ar:
+        if not order:
+            return ""
+        first = dict([(x,x) for x in order])
+    else :
+        first = ar[0]
+    if header: #insert a new row of just the keys, the next loop will do the rest
+        new = dict([(x,str(x).capitalize()) for x in first.keys()]) 
+        new = [new]
+    else :
+        new = []
+    #figure out what the longest column in each row is
+    maxes = dict([(a,len(str(b))) for a,b in first.items()])
+    for x in new + ar:
+        for k, v in x.items():
+            size = len(str(v))
+            if size > maxes[k]:
+                maxes[k] = size
+    
+    maxesfmt = dict([(x, "%%-%ds" % (y+1)) for x,y in  maxes.items()])
+    if order:
+        it = order
+    else :
+        it = first #will iterate over the keys however
+    
+    table = []
+
+    for x in new + ar:
+        row = []
+        for c in it:
+            row.append( maxesfmt[c] % x[c] )
+        line = " ".join(row).strip()
+        table.append(line)
+
+    return "\n".join(table)
