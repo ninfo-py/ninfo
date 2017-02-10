32fe8e1ab3c1d49cf202a319f8c3302dad9a1098
Justin Azoff <justin.azoff@gmail.com>
2017-02-10 14:30:58 -0500

    bump version to 0.6.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='ninfo',
-    version='0.5.2',
+    version='0.6.0',
     zip_safe=False,
     packages = find_packages(exclude=["tests"]),
     include_package_data=True,
