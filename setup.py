b7bfda137ae5635eecc04632b07376787577a8e3
Ryan Goggin <support@ryangoggin.net>
2021-05-21 16:11:37 -0400

    Update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='ninfo',
-    version='0.6.3',
+    version='0.7.0',
     zip_safe=False,
     packages = find_packages(exclude=["tests"]),
     include_package_data=True,
